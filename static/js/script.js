451ee7d77a5012beea5c81bb0534b10490ffc983
harryer68 <harryer@naver.com>
2024-09-11 15:46:39 +0900

    mp4 변환

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
 document.getElementById('myButton').addEventListener('click', function() {
     alert('버튼이 클릭되었습니다!');
 });
+<script src="https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.11.6/dist/ffmpeg.min.js"></script>
