e6e8f2d4c611d8c8822a145d2268ed3154434a01
harryer68 <harryer@naver.com>
2024-09-11 15:46:48 +0900

    커밋

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
 document.getElementById('myButton').addEventListener('click', function() {
     alert('버튼이 클릭되었습니다!');
 });
-<script src="https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.11.6/dist/ffmpeg.min.js"></script>
